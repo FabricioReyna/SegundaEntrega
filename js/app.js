14d69e938618f8e08075c906c3a95cd4e1164071
Frontend_boantek <fabricioreynacordes@gmail.com>
2022-10-18 23:31:19 -0300

    Reciente

---
+++ js/app.js
@@ -0,0 +1,44 @@
+let body = document.getElementById("body")
+let nombre = prompt("Hola bienvenido porfavor introduzca su nombre asi se a quien me dirijo")
+    if(nombre == ""){
+        body.innerHTML = ` <div><h1 class="center">Bienvenido Usuario</h1></div>
+        <div class="card-padre"><div class="card ey" style="width: 18rem;" id="card">
+<img src="https://jumboargentina.vtexassets.com/arquivos/ids/711224/Yerba-Mate-Playadito-Suave-X1kg-1-854539.jpg?v=637938633804770000" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">Card title</h5>
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>`
+console.log(body)
+
+    } else{
+body.innerHTML = ` <div><h1 class="center">Bienvenido ${nombre}</h1></div>
+<div class="card-padre"><div class="card ey" style="width: 18rem;" id="card">
+<img src="https://jumboargentina.vtexassets.com/arquivos/ids/711224/Yerba-Mate-Playadito-Suave-X1kg-1-854539.jpg?v=637938633804770000" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">Card title</h5>
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>`
+    }
+
+let card = document.getElementById("card")
+
+card.innerHTML=`
+    <div>
+    <img src="https://jumboargentina.vtexassets.com/arquivos/ids/711224/Yerba-Mate-Playadito-Suave-X1kg-1-854539.jpg?v=637938633804770000" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    <div>
+    <img src="https://jumboargentina.vtexassets.com/arquivos/ids/711224/Yerba-Mate-Playadito-Suave-X1kg-1-854539.jpg?v=637938633804770000" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  `
